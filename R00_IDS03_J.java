f47755d04f615a71e7f1246a7f3f44787768e5b6
esaenz09 <77026390+esaenz09@users.noreply.github.com>
2023-10-05 19:58:44 -0400

    Updated mandatory examples to be compliant

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -1,16 +1,21 @@
 import java.util.logging.Logger;
+import java.util.regex.Pattern;
 
 public class R00_IDS03_J {
     private static final Logger logger = Logger.getLogger(R00_IDS03_J.class.getName());
-    
-    public static void main(String[] args) {
+
+    public String sanitizeUser(String username) {
+        return Pattern.matches("[A-Za-z0-9_]+", username) ? username : "unauthorized user";
+    }
+    public void main(String[] args) {
         String username = "";
         Boolean loginSuccessful = true;
 
         if (loginSuccessful) {
-            logger.severe("User login succeeded for: " + username);
+            logger.severe("User login succeeded for: " + sanitizeUser(username));
         } else {
-            logger.severe("User login failed for: " + username);
+            logger.severe("User login failed for: " + sanitizeUser(username));
         }
     }
+
 }
